40eb1513906b47c7e8440c07a9039bb8f1058859
ANONYMOUS-U7P4L <123867620+ANONYMOUS-U7P4L@users.noreply.github.com>
2023-02-11 18:55:05 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -368,7 +368,7 @@ function print_banner() {
     printf "${blue}##  88     '88.   d8'        '8b  88        88  ##\n"
     printf "${blue}##  88       Y8b d8'          '8b 888888888 88  ##\n"
     printf "${blue}##                                              ##\n"  
-    printf "${yellow}##                 Re-developed by ANONYMOUS U7P4L ##\n"
+    printf "${yellow}##            Re-developed by ANONYMOUS U7P4L ##\n"
     printf "${blue}####  ############# NetHunter ####################${reset}\n\n"
 }
 
